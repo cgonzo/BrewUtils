20c62e3e1f74234566460912b278ab9a8444604d
Chris Gonzales <cgonzo86@gmail.com>
2014-10-24 16:12:36 -0400

    initial commit of scale.py

---
+++ scale.py
@@ -0,0 +1,45 @@
+import hid
+import time
+
+def scaleInit:
+    global h
+    try:
+        print "Opening device"
+        h = hid.device()
+        h.open(0x4d9, 0x8010)
+    
+        print "Manufacturer: %s" % h.get_manufacturer_string()
+        print "Product: %s" % h.get_product_string()
+        print "Serial No: %s" % h.get_serial_number_string()
+        return 1
+    except IOError, ex:
+        print ex
+        print "You probably don't have the hard coded test hid. Update the hid.device line"
+        print "in this script with one from the enumeration list output above and try again."
+        return 0
+
+def scaleOn:
+    h.send_feature_report([0, 0xa0, 0xde, 0xa0, 0, 0, 0, 0, 0])
+
+def scaleRead:
+    h.send_feature_report([0x00, 0x90, 0xfe, 0x80, 0xff, 0x7f, 0x00, 0x00, 0x03])
+    return h.read(9)
+
+def rawdata2g( arr ):
+    return (arr[2]>>4 & 0xf)*10000+(arr[2] & 0xf)*1000+(arr[1]>>4 & 0xf)*100+(arr[1] & 0xf)*10
+
+
+scaleInit()
+scaleOn()
+while 1:
+    data = scaleRead()
+    print data
+    print rawdata2g(data)
+    time.sleep(0.05)
+
+    
+print "Closing device"
+h.close()
+
+
+print "Done"
