b51e5874ca03e464255fbedc8f92690154931fe5
Chris Gonzales <cgonzo86@gmail.com>
2014-10-26 13:53:26 -0400

    add support for weight to utils

--- utils.py
+++ utils.py
@@ -3,10 +3,14 @@
 import cgi
 import cgitb
 from thermometer import thermometerRead
+from scale import *
 
 form = cgi.FieldStorage()
 function=form.getvalue("function")
 
 if (function == "temperature"): 
   print(thermometerRead("28-00042d367bff"))
+
+if (function == "weight"): 
+  print(rawdata2g(scaleRead()))
 
