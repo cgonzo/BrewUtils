593e8384f9d0442eaf974a13e5008a4cc5a332e9
Chris Gonzales <cgonzo86@gmail.com>
2014-11-08 08:57:03 -0500

    Add correct tare and full weight

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-var targetTemperature=38;
-var topTemperature=50;
-var bottomTemperature=32;
-var tareWeight=13;
-var fullWeight=58;
+var targetTemperature=20;
+var topTemperature=25;
+var bottomTemperature=0;
+var tareWeight=3520;
+var fullWeight=8100;
